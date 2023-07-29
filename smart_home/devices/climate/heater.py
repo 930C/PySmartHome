527e2802c81a469959390ef71dc37ea943698467
Lisa Ress-Park <s212547@student.dhbw-mannheim.de>
2023-07-29 12:26:06 +0200

    little change in heating

--- smart_home/devices/climate/heater.py
+++ smart_home/devices/climate/heater.py
@@ -9,7 +9,7 @@ class Heater(AdjustableDevice, TemperatureControlInterface):
         self.logger.info(f'Heater {name} created with initial level {initial_level}')
 
     def heat(self):
-        self.logger.info(f'Heating {self.name} with step 0.1')
+        self.logger.info(f'Heating system {self.name} with step 0.1')
         if not self.get_state():
             self.turn_on()
         self.increase_level(0.1)
